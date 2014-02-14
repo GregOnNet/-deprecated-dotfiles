35983394947aef22132f5b1a8e07bd6cf2e61f46
Gregor Woiwode <gregor.woiwode@o-s.de>
2014-02-14 11:36:22 +0100

    added function `st` to edit files with sublime

--- bash/functions.sh
+++ bash/functions.sh
@@ -2,6 +2,10 @@ function ui {
   "/cygdrive/c/Program Files (x86)/GitExtensions/GitExtensions.exe" $@ &	
 }
 
+function st {
+  "/cygdrive/c/Program Files/Sublime Text 3/sublime_text.exe" `cygpath $1` &
+}
+
 function up {
   cd ..
 }
