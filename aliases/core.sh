d078ed3c95353d508b1bb86b1f2a24638f8092d1
Gregor Woiwode <gregor.woiwode@o-s.de>
2014-02-15 20:59:45 +0100

    added core aliases for grep and directory listings

---
+++ aliases/core.sh
@@ -0,0 +1,6 @@
+# grep
+  alias grep='grep --color'                     # show differences in colour
+
+# directory listings
+  alias ls='ls -hF --color=tty'                 # classify files in colour
+  alias la='ls -A'                              # all but . and ..
