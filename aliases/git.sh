4d9cd9cc48e4bb3be8830cb208b8e59b58323c8d
Gregor Woiwode <gregor.woiwode@gmail.com>
2014-05-19 10:27:40 +0200

    Added alias preview displaying diff between index and local history (commited stuff).

--- aliases/git.sh
+++ aliases/git.sh
@@ -8,6 +8,7 @@
   alias d='git diff '
   alias l='git log --graph --abbrev-commit --decorate --date=relative --format=format:"%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)"'
   alias m='git merge '
+  alias preview='git diff --cached'
   alias r='git rm '
   alias reword='git commit --amend -m'
   alias s='git status'
